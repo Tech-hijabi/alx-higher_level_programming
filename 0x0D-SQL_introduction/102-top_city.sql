6472af3c5a05b44a67d6b04db94ec192a9313e59
Barakat Mohammed <adeyemoadeshewa10@gmail.com>
2024-01-16 20:27:34 -0800

    Advanced tasks 101, 102 & 103 code fix

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -1,5 +1,5 @@
 -- Task: Display the top 3 cities' temperatures during July and August ordered by temperature (descending)
-SELECT city, AVG(temp_fahrenheit) AS avg_temp
+SELECT city, AVG(value) AS avg_temp
 FROM temperatures
 WHERE MONTH(date) IN (7, 8) -- July and August
 GROUP BY city
--- 0x0D-SQL_introduction/103-max_state.sql
+++ 0x0D-SQL_introduction/103-max_state.sql
@@ -1,5 +1,5 @@
 -- Task: Display the max temperature of each state ordered by State name
-SELECT state, MAX(temp_fahrenheit) AS max_temp
+SELECT state, MAX(value) AS max_temp
 FROM temperatures
 GROUP BY state
 ORDER BY state;
